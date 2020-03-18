36758125ec28a8a04415088d09a84072606c1103
Angel Esquinas <anllelonair@gmail.com>
2020-03-18 20:36:26 +0100

    Todos los planetas añadidos con su imagen

---
+++ app/src/main/java/com/example/planetario/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,27 @@
+package com.example.planetario.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.planetario.R;
+
+public class SlideshowFragment extends Fragment {
+
+    private SlideshowViewModel slideshowViewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
+    }
+}
